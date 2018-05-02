3bb88c71007b5c8e750379582457c59c037da68a
Alex Martin <alexmartinturrillas@gmail.com>
2018-05-02 15:19:17 +0200

    one class added

--- one-icon.js
+++ one-icon.js
@@ -1,10 +1,11 @@
 import {oneIconset} from "./one-iconset.js"
-import {html, LitElement} from "@polymer/lit-element"
+import {html, OneClass} from "@alexmtur/one-class"
 
-export class OneIcon extends LitElement {
+export class OneIcon extends OneClass {
     static get properties() {return {
         icon: {type: String, value: "close"},
         size: {type: String, value: "25px"},
+        padding: {type: String, value: "0"},
         viewBox: {type: String, value: "0 0 100 100"},
         // width: {type: String, value: "25px"},
         // height: {type: String, value: "auto"}
@@ -13,9 +14,11 @@ export class OneIcon extends LitElement {
         super();
         this.icon = 'close';
         this.viewBox = '0 0 100 100';
+        this.padding = '0';
+        this.size = '25px';
     }
     //Gradient definitions have to be put inside the one-style element
-    render() {return html`
+    _render() {return html`
     <!-- <svg class="one-icon" style="width:0;height:0;position:absolute;" aria-hidden="true" focusable="false">
           <defs>
             <linearGradient id="grad1" x1="0%" y1="0%" x2="100%" y2="100%">
@@ -24,7 +27,7 @@ export class OneIcon extends LitElement {
             </linearGradient>
           </defs>
         </svg> -->
-        <svg viewBox="0 0 100 100" width$="${this.size}" style="padding: 5px">
+        <svg viewBox="0 0 100 100" width$="${this.size}" style="padding: ${this.padding}">
           ${oneIconset[this.icon]}
         </svg>`;
     }
